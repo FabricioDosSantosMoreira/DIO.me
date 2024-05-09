7f60d512c1c27a4db310328d408366e8964ba34e
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-05-08 23:37:22 -0300

    ADD Python - Essencial course to Python AI Backend Developer

---
+++ Python AI Backend Developer/01 - Python - Essencial/01 - Essencial/Módulo 01 - Conhecendo o Python/02_funcoes_de_entrada_e_saida.py
@@ -0,0 +1,30 @@
+# - - - - -> Funções de Entrada e Saída:
+
+# - - - -> input():
+
+    # A função builtin 'input' do python é utilizada quando queremos ler dados da entrada padrão. 
+    # Ela recebe um argumento do tipo string, que é exibido para o usuário na saída padrão. 
+    # A função lê a entrada, converte para string e retorna o valor.
+
+# - - - -> print():
+
+    # A função builtin 'print' do python é utilizada quando queremos exibir dados na saída padrão. 
+    # Ela recebe um argumento obrigatório do tipo varargs de objetos e outros 4 argumentos 
+    # opcionais (sep, end, file e flush). Todos os objetos são convertidos para string. 
+    # A string final é exibida para o usuário.
+
+
+nome = input("\nInforme o seu nome: ")
+
+idade = input("\nInforme a sua idade: ")
+
+
+print()
+
+print(nome, idade)
+
+print(nome, idade, sep="-")
+
+print(nome, idade, end=".\n")
+
+print(nome, idade, sep="-", end=".\n")
---
+++ Python AI Backend Developer/01 - Python - Essencial/01 - Essencial/Módulo 01 - Conhecendo o Python/05_conversao_de_tipos_de_dados.py
@@ -0,0 +1,72 @@
+# - - - - -> Conversão de Tipos de Dados:
+
+    # Conversão de tipos de dados é o processo de transformar um tipo de dado em outro.
+    # Isso é útil para realizar operações que envolvem diferentes tipos de dados ou 
+    # para garantir a compatibilidade entre eles.
+
+
+# - - - -> Conversão para Inteiro (int):
+valor_str = "10"
+valor_int_convertido = int(valor_str)
+print("\nConversão para Inteiro (int):", valor_int_convertido)
+print(type(valor_int_convertido))
+
+# - - - -> Conversão para Ponto Flutuante (float):
+valor_str = "3.14"
+valor_float_convertido = float(valor_str)
+print("\nConversão para Ponto Flutuante (float):", valor_float_convertido)
+print(type(valor_float_convertido))
+
+# - - - -> Conversão para Cadeia de Caracteres (string):
+valor_int = 10
+valor_str_convertido = str(valor_int)
+print("\nConversão para Cadeia de Caracteres (string):", valor_str_convertido)
+print(type(valor_str_convertido))
+
+# - - - -> Conversão para Booleano (bool):
+valor_int = 0
+valor_booleano_convertido = bool(valor_int)
+print("\nConversão para Booleano (bool):", valor_booleano_convertido)
+print(type(valor_booleano_convertido))
+
+# - - - -> Conversão para Lista (list):
+valor_tupla = (1, 2, 3, 4, 5)
+valor_lista_convertida = list(valor_tupla)
+print("\nConversão para Lista (list):", valor_lista_convertida)
+print(type(valor_lista_convertida))
+
+# - - - -> Conversão para Tupla (tuple):
+valor_lista = [1, 2, 3, 4, 5]
+valor_tupla_convertida = tuple(valor_lista)
+print("\nConversão para Tupla (tuple):", valor_tupla_convertida)
+print(type(valor_tupla_convertida))
+
+# - - - -> Conversão para Dicionário (dictionary):
+valor_lista_de_listas = [['a', 1], ['b', 2], ['c', 3]]
+valor_dicionario_convertido = dict(valor_lista_de_listas)
+print("\nConversão para Dicionário (dictionary):", valor_dicionario_convertido)
+print(type(valor_dicionario_convertido))
+
+# - - - -> Conversão para Conjunto (set):
+valor_lista = [1, 2, 3, 4, 5, 5]
+valor_conjunto_convertido = set(valor_lista)
+print("\nConversão para Conjunto (set):", valor_conjunto_convertido)
+print(type(valor_conjunto_convertido))
+
+# - - - -> Conversão para nulo (None):
+valor_str = ""
+valor_nulo_convertido = None if valor_str == "" else valor_str
+print("\nConversão para nulo (None):", valor_nulo_convertido)
+print(type(valor_nulo_convertido))
+
+# - - - -> Conversão para Bytes (bytes):
+valor_str = "Hello"
+valor_bytes_convertido = bytes(valor_str, 'utf-8')
+print("\nConversão para Bytes (bytes):", valor_bytes_convertido)
+print(type(valor_bytes_convertido))
+
+# - - - -> Conversão para Bytearray (bytearray):
+valor_bytes = b'Hello'
+valor_bytearray_convertido = bytearray(valor_bytes)
+print("\nConversão para Bytearray (bytearray):", valor_bytearray_convertido)
+print(type(valor_bytearray_convertido))
