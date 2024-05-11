ee93d75d006def0eff61e7bb89e39e98f3bc56ff
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-05-10 22:16:34 -0300

    ADD: Python - Essencial

---
+++ Cursos/01 - Python AI Backend Developer/01 - Python - Essencial/Módulo 01 - Conhecendo o Python/03_variaveis_e_constantes.py
@@ -0,0 +1,34 @@
+# - - - - -> Variáveis e Constantes:
+
+    # - - - -> Variáveis:
+        #   Variáveis são espaços de memória reservados para armazenar dados manipuláveis 
+        # durante a execução do programa. Elas possuem um nome que as identifica e 
+        # um valor associado que pode ser modificado ao longo do programa.
+
+    # - - - -> Constantes:
+        #   Constantes, são valores que não podem ser alterados durante a execução do programa. 
+        # No Python, não existe um tipo de dado específico para constantes, mas a convenção 
+        # é que nomes de variáveis em letras maiúsculas sejam tratados como constantes, 
+        # indicando que seus valores não devem ser modificados.
+
+
+nome: str = "Fabrício"  # Atribuição de variável do tipo str
+idade: int = 19 # Atribuição de variável do tipo int
+altura: float = 1.90 # Atribuição de variável do tipo float
+
+print(nome, idade, altura)
+
+nome, idade, altura = "Teste", 30, 1.60 # Atribuição de mútiplas variáveis em uma linha
+
+print(nome, idade, altura)
+
+
+ESTADOS_BRASILEIROS: list = ["SP", "RJ", "SC", "RS"] # Atribuição de constante do tipo list
+
+EXECUTAR, VERIFICAR = True, False # Atribuição de mútiplas constantes em uma linha
+
+print(ESTADOS_BRASILEIROS)
+
+print(EXECUTAR)
+
+print(VERIFICAR)
---
+++ Cursos/01 - Python AI Backend Developer/01 - Python - Essencial/Módulo 01 - Conhecendo o Python/04_tipos_de_dados.py
@@ -0,0 +1,62 @@
+# - - - - -> Tipos de Dados:
+
+    #   Tipos de dados são categorias que especificam quais valores podem ser armazenados e manipulados. 
+    # Esses tipos de dados são fundamentais para manipular informações e realizar operações. 
+    # Cada tipo de dado possui características específicas e métodos associados 
+    # para manipulação e processamento.
+
+
+    # - - - -> Tipo de dado: Inteiro (integer)
+valor_int: int = 10
+print("Inteiro (integer):", valor_int)
+print(type(valor_int))
+
+    # - - - -> Tipo de dado: Ponto Flutuante (float)
+valor_float = 3.14
+print("\nPonto Flutuante (float):", valor_float)
+print(type(valor_float))
+
+    # - - - -> Tipo de dado: Cadeia de Caracteres (string)
+valor_str: str = "Olá, mundo!"
+print("\nCadeia de Caracteres (string):", str)
+print(type(valor_str))
+
+    # - - - -> Tipo de dado: Booleano (bool)
+valor_booleano = True
+print("\nBooleano (bool):", valor_booleano)
+print(type(valor_booleano))
+
+    # - - - -> Tipo de dado: Lista (list)
+valor_lista = [1, 2, 3, 4, 5]
+print("\nLista (list):", valor_lista)
+print(type(valor_lista))
+
+    # - - - -> Tipo de dado: Tupla (tuple)
+valor_tupla = (1, 2, 3, 4, 5)
+print("\nTupla (tuple):", valor_tupla)
+print(type(valor_tupla))
+
+    # - - - -> Tipo de dado: Dicionário (dictionary)
+valor_dicionario = {'a': 1, 'b': 2, 'c': 3}
+print("\nDicionário (dictionary):", valor_dicionario)
+print(type(valor_dicionario))
+
+    # - - - -> Tipo de dado: Conjunto (set)
+valor_conjunto = {1, 2, 3, 4, 5}
+print("\nConjunto (set):", valor_conjunto)
+print(type(valor_conjunto))
+
+    # - - - -> Tipo de dado: nulo (None)
+valor_nulo = None
+print("\nNulo (None):", valor_nulo)
+print(type(valor_nulo))
+
+    # - - - -> Tipo de dado: Bytes (bytes)
+valor_bytes: bytes = b'Hello'
+print("\nBytes (bytes):", valor_bytes)
+print(type(valor_bytes))
+
+    # - - - -> Tipo de dado: Bytearray (bytearray)
+valor_bytearray: bytearray = bytearray(b'Hello')
+print("\nBytearray (bytearray):", valor_bytearray)
+print(type(valor_bytearray))
