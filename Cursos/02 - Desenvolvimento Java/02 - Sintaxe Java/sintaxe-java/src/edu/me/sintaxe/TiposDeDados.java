6101ae7d1bee2f7818936b4b2cfcff8fb7c9b51e
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-06-27 17:40:27 -0300

    ADD: Desenvolvimento Java

---
+++ Cursos/02 - Desenvolvimento Java/02 - Sintaxe Java/sintaxe-java/src/edu/me/sintaxe/TiposDeDados.java
@@ -0,0 +1,32 @@
+// - - - - -> Tipos de Dados:
+
+ 
+
+
+
+
+
+
+package edu.me.sintaxe;
+
+public class TiposDeDados {
+
+    public static void main(String[] args) throws Exception {
+
+
+        double salarioMinimo = 2500.33;
+
+        System.out.println("Hello, World!");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
---
+++ Cursos/02 - Desenvolvimento Java/02 - Sintaxe Java/sintaxe-java/src/edu/me/sintaxe/VariaveisEConstantes.java
@@ -0,0 +1,5 @@
+package edu.me.sintaxe;
+
+public class VariaveisEConstantes {
+
+}
