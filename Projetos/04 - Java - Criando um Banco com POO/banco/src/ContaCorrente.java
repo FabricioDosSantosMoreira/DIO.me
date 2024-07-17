d00eab610b78fbadc6a89b6ac596d7fac87f02b5
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-07-17 15:42:41 -0300

    UPDATE: Desenvolvimento Java

---
+++ Projetos/04 - Java - Criando um Banco com POO/banco/src/ContaCorrente.java
@@ -0,0 +1,34 @@
+public class ContaCorrente {
+
+    // Atributos da classe 'ContaCorrente'
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+}
