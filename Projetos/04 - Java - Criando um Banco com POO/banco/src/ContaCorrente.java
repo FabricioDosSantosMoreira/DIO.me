8716baa00b5656a3711776206bb41a39ce1fee0e
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-07-17 17:11:43 -0300

    UPDATE: Desenvolvimento Java

--- Projetos/04 - Java - Criando um Banco com POO/banco/src/ContaCorrente.java
+++ Projetos/04 - Java - Criando um Banco com POO/banco/src/ContaCorrente.java
@@ -1,34 +1,8 @@
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
 
-    // Atributos da classe 'ContaCorrente'
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-
-    public void sacar() {
-
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("\nExtrato Conta Corrente");
+        super.exibirInfo();
     }
-
-    public void depositar() {
-
-    }
-
-    public void transferir() {
-
-    }
-
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
 }
--- Projetos/04 - Java - Criando um Banco com POO/banco/src/ContaPoupanca.java
+++ Projetos/04 - Java - Criando um Banco com POO/banco/src/ContaPoupanca.java
@@ -1,3 +1,8 @@
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta {
 
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("\nExtrato Conta Poupança");
+        super.exibirInfo();
+    }
 }
