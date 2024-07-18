a0a94ad87e0bdf84bd7a02bd2139e53a55e3bc65
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-07-18 19:17:17 -0300

    UPDATE: Desenvolvimento Java

---
+++ Cursos/02 - Desenvolvimento Java/09 - Adicionando Segurança a uma REST API com Spring Security/02 - Configure Adapter/java-project/src/main/java/edu/me/springboot/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package edu.me.springboot;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override 
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            //.antMatchers(HttpMethod.GET, "/").permitAll()
+            .antMatchers(HttpMethod.POST, "/login").permitAll()
+            .antMatchers(HttpMethod.GET, "/managers").hasAnyRole("MANAGERS")
+            .antMatchers(HttpMethod.GET, "/users").hasAnyRole("USERS", "MANAGERS")
+            .anyRequest().authenticated().and().formLogin();
+    }
+    
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+            .password("{noop}123456")
+            .roles("USERS")
+            .and()
+            .withUser("admin")
+            .password("{noop}admin")
+            .roles("MANAGERS");
+    }
+}
