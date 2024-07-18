1e9ef5fc0173948adcf039366f5d262f7576a60c
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-07-18 16:55:32 -0300

    UPDATE: Desenvolvimento Java

--- Projetos/05 - Java - Criando uma REST API/java-project/src/main/java/edu/me/springboot/controller/UserController.java
+++ Projetos/05 - Java - Criando uma REST API/java-project/src/main/java/edu/me/springboot/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,11 @@ public class UserController {
 
     @PostMapping()
     public void postUser(@RequestBody User user) {
+        repository.save(user);
+    }
+
+    @PutMapping
+    public void putUser(@RequestBody User user) {
         repository.save(user);
     }
 
