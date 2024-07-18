ae2c135f28a64bf09a70a205ede592715ff1acbf
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-07-18 16:28:42 -0300

    UPDATE: Desenvolvimento Java

--- Projetos/05 - Java - Criando uma REST API/java-project/src/main/java/edu/me/springboot/Application.java
+++ Projetos/05 - Java - Criando uma REST API/java-project/src/main/java/edu/me/springboot/Application.java
@@ -1,13 +1,15 @@
 package edu.me.springboot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SpringBootRestApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ Projetos/05 - Java - Criando uma REST API/java-project/src/main/java/edu/me/springboot/repository/UserRepository.java
@@ -0,0 +1,51 @@
+package edu.me.springboot.repository;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import edu.me.springboot.model.User;
+
+
+@Repository
+public class UserRepository {
+
+    public void save(User user) {
+
+        if(user.getId() == null) {
+            System.out.println("\n[SAVE] - Saving user");
+        } else {
+            System.out.println("\n[UPDATE] - Updating user");
+        }
+
+        System.out.println("\nUSER: " + user);
+    }
+
+
+    public void deleteById(Integer id) {
+        System.out.println("\n[DELETE/id] - id=" + id);
+    }
+
+
+    public List<User> findAll() {
+        System.out.println("\n[LIST] - Listing all users");
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("teste1", "teste1senha"));
+        users.add(new User("teste2", "teste2senha"));
+        users.add(new User("teste3", "teste3senha"));
+
+        return users;
+    }
+
+    public User findById(Integer id) {
+        System.out.println("\n[FIND/id] - Searching user");
+        return new User("teste", "teste");
+    }
+
+    public User findByUsername(String username) {
+        System.out.println("\n[FIND/username] - Searching user");
+        return new User("teste", "teste");
+    }
+    
+}
