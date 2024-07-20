f747d09ab27edc27e431756ce08cedafe569510a
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-07-20 16:05:23 -0300

    UPDATE: Desenvolvimento Java

---
+++ Projetos/06 - Java - RESTful API - Spring Boot 3 - Java 17 - Railway/java-rest-api/src/test/java/edu/me/JavaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
