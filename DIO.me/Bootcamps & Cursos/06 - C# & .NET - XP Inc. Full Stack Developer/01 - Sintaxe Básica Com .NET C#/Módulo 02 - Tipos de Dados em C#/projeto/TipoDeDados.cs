76747ff87ae8e33f5a73badf73284e3fb3539766
Fabrício Dos Santos Moreira <dev.fabriciodossantosmoreira@gmail.com>
2024-09-03 20:20:28 -0300

    [ADD] - C# Bootcamp

---
+++ DIO.me/Bootcamps & Cursos/06 - C# & .NET - XP Inc. Full Stack Developer/01 - Sintaxe Básica Com .NET C#/Módulo 02 - Tipos de Dados em C#/projeto/TipoDeDados.cs
@@ -0,0 +1,41 @@
+﻿{
+string variavel_string = "valor";
+
+int variavel_int = 1;
+long variavel_long = 100000000L;
+uint variavel__uint = 10000;
+byte variavel_byte = 255;
+ulong variavel_ulong = 13931231841L;
+
+decimal variavel_decimal = 18080310M;
+double variavel_double = 1.80;
+float variavel_float = 10.1240F;
+
+bool variavel_bool = true;
+
+Console.WriteLine(variavel_string);
+Console.WriteLine(variavel_int);
+Console.WriteLine(variavel_long);
+Console.WriteLine(variavel__uint);
+Console.WriteLine(variavel_byte);
+Console.WriteLine(variavel_ulong);
+Console.WriteLine(variavel_decimal);
+Console.WriteLine(variavel_double);
+Console.WriteLine(variavel_double.ToString("0.00"));
+Console.WriteLine(variavel_float);
+Console.WriteLine(variavel_bool);
+
+
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual);
+
+string dataAtualString = DateTime.Now.ToString("dd/MM/yyyy - HH:mm");
+Console.WriteLine(dataAtualString);
+
+dataAtualString = DateTime.Now.ToString("dd/MM - HH:mm");
+Console.WriteLine(dataAtualString);
+}
---
+++ DIO.me/Bootcamps & Cursos/06 - C# & .NET - XP Inc. Full Stack Developer/01 - Sintaxe Básica Com .NET C#/Módulo 03 - Tipos de Operadores em C#/Operadores Condicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OperadoresCondicionais
+{
+    public class OperadoresCondicionais
+    {
+        public void VerificarEstoque()
+        {
+            int quantidadeCompra = 10;
+            int quantidadeEstoque = 20;
+
+            Console.WriteLine($"Quantidade em Estoque: {quantidadeEstoque}");
+            Console.WriteLine($"Quantidade Compra: {quantidadeCompra}");
+
+            if (quantidadeEstoque >= quantidadeCompra)
+            {
+                Console.WriteLine("Item Comprado!");
+                quantidadeEstoque -= quantidadeCompra;
+            }
+            else
+            {
+                Console.WriteLine("Faltou Estoque!");
+            }
+
+            Console.WriteLine($"Quantidade em Estoque: {quantidadeEstoque}");
+            Console.WriteLine($"Quantidade Compra: {quantidadeCompra}");
+        }
+
+        public static void Main(string[] args)
+        {
+            OperadoresCondicionais operadores = new OperadoresCondicionais();
+            operadores.VerificarEstoque();
+        }
+    }
+}
