9f5e5ebab8f7997e2cfe43985832a580b560f892
Fabrício Dos Santos Moreira <dev.fabriciodossantosmoreira@gmail.com>
2024-09-06 16:26:36 -0300

    [ADD] - Projeto Hospedagem

---
+++ DIO.me/Projetos/13 - C# & .NET - Construindo um SIstema de Hospedagem de um Hotel no C#/ProjetoHospedagem/Models/Reserva.cs
@@ -0,0 +1,55 @@
+namespace ProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("A capacidade da suíte é inferior ao número de hóspedes!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 0.10M;
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ DIO.me/Projetos/13 - C# & .NET - Construindo um SIstema de Hospedagem de um Hotel no C#/ProjetoHospedagem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using ProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+// Cria a suíte
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+
+
+
+
+
+
+
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes2 = new List<Pessoa>();
+
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+Pessoa p4 = new Pessoa(nome: "Hóspede 4");
+
+hospedes2.Add(p3);
+hospedes2.Add(p4);
+
+// Cria a suíte
+Suite suite2 = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 20);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva2 = new Reserva(diasReservados: 11);
+reserva2.CadastrarSuite(suite2);
+reserva2.CadastrarHospedes(hospedes2);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva2.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva2.CalcularValorDiaria()}");
